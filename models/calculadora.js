51459885bc0977cd16bb89cdc9a5d549219f867a
Joao-Neto15 <152300254+Joao-Neto15@users.noreply.github.com>
2024-01-31 18:54:23 +0000

    dia 3

---
+++ models/calculadora.js
@@ -0,0 +1,5 @@
+function somar(numero1, numero2) {
+  return numero1 + numero2;
+}
+
+exports.somar = somar;
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,1 +1,10 @@
-test("nome do test", callbackfunction);
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+test("somar 5 + 100 deveria retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
